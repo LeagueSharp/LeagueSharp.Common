3daffa542c913c4f8b13bde8ef7f7f27b36b738b
Esk0r <kortatu1512@hotmail.com>
2014-08-20 17:39:56 +0200

    Fixed DX onreset bugsplats

--- LeagueSharp.Common/LeagueSharp.Common/Render.cs
+++ LeagueSharp.Common/LeagueSharp.Common/Render.cs
@@ -1,4 +1,5 @@
 ﻿#region LICENSE
+
 /*
  Copyright 2014 - 2014 LeagueSharp
  Orbwalking.cs is part of LeagueSharp.Common.
@@ -16,6 +17,7 @@
  You should have received a copy of the GNU General Public License
  along with LeagueSharp.Common. If not, see <http://www.gnu.org/licenses/>.
 */
+
 #endregion
 
 #region
@@ -26,6 +28,7 @@ using System.Drawing;
 
 using SharpDX;
 using SharpDX.Direct3D9;
+using Font = SharpDX.Direct3D9.Font;
 
 #endregion
 
@@ -41,6 +44,24 @@ namespace LeagueSharp.Common
         static Render()
         {
             Drawing.OnEndScene += Drawing_OnEndScene;
+            Drawing.OnPreReset += DrawingOnOnPreReset;
+            AppDomain.CurrentDomain.DomainUnload += CurrentDomainOnDomainUnload;
+        }
+
+        private static void CurrentDomainOnDomainUnload(object sender, EventArgs eventArgs)
+        {
+            foreach (var renderObject in RenderObjects)
+            {
+                renderObject.OnUnload();
+            }
+        }
+
+        private static void DrawingOnOnPreReset(EventArgs args)
+        {
+            foreach (var renderObject in RenderObjects)
+            {
+                renderObject.OnPreReset();
+            }
         }
 
         private static void Drawing_OnEndScene(EventArgs args)
@@ -73,30 +94,153 @@ namespace LeagueSharp.Common
             return renderObject;
         }
 
+        public class Rectangle : RenderObject
+        {
+            private readonly Line _line;
+            public ColorBGRA Color;
+
+            public Rectangle(int x, int y, int width, int height, ColorBGRA color)
+            {
+                X = x;
+                Y = y;
+                Width = width;
+                Height = height;
+                Color = color;
+                _line = new Line(Drawing.Direct3DDevice);
+                _line.Width = height;
+            }
+
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int Width { get; set; }
+            public int Height { get; set; }
+
+            public override void OnEndScene()
+            {
+                try
+                {
+                    if (_line.IsDisposed)
+                    {
+                        return;
+                    }
+                    _line.Begin();
+                    _line.Draw(new[] { new Vector2(X, Y + Height / 2), new Vector2(X + Width, Y + Height / 2) }, Color);
+                    _line.End();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Common.Render.Rectangle.OnEndScene: " + e);
+                }
+            }
+
+            public override void OnPreReset()
+            {
+                _line.OnLostDevice();
+            }
+
+            public override void OnUnload()
+            {
+                _line.Dispose();
+            }
+        }
+
         public class RenderObject
         {
             public int Layer = 0;
             public bool Visible = true;
 
             public virtual void OnEndScene() {}
+            public virtual void OnPreReset() {}
+
+            public virtual void OnUnload() {}
+        }
+
+        public class Sprite : RenderObject
+        {
+            private readonly SharpDX.Direct3D9.Sprite _sprite;
+            public int X = 0;
+            public int Y = 0;
+            private int _height;
+            private Texture _texture;
+            private int _width;
+
+            public Sprite(int x, int y, Bitmap bitmap, int width = -1, int height = -1)
+            {
+                X = x;
+                Y = y;
+                _width = width;
+                _height = height;
+                Bitmap = bitmap;
+                _sprite = new SharpDX.Direct3D9.Sprite(Drawing.Direct3DDevice);
+                _texture = Texture.FromMemory(
+                    Drawing.Direct3DDevice, (byte[]) new ImageConverter().ConvertTo(bitmap, typeof(byte[])),
+                    width != -1 ? width : bitmap.Width, height != -1 ? height : bitmap.Height, 0, Usage.None, Format.A1,
+                    Pool.Managed, Filter.Default, Filter.Default, 0);
+            }
+
+            public int Width
+            {
+                set { _width = value; }
+                get { return _width > 0 ? _width : Bitmap.Width; }
+            }
+
+            public int Height
+            {
+                set { _height = value; }
+                get { return _height > 0 ? _height : Bitmap.Height; }
+            }
+
+            public Bitmap Bitmap { get; set; }
+
+            public void UpdateTextureBitmap(Bitmap newBitmap)
+            {
+                Bitmap = newBitmap;
+                _texture = Texture.FromMemory(
+                    Drawing.Direct3DDevice, (byte[]) new ImageConverter().ConvertTo(newBitmap, typeof(byte[])), Width,
+                    Height, 0, Usage.None, Format.A1, Pool.SystemMemory, Filter.Default, Filter.Default, 0);
+            }
+
+            public override void OnEndScene()
+            {
+                try
+                {
+                    if (_sprite.IsDisposed || _texture.IsDisposed)
+                    {
+                        return;
+                    }
+                    _sprite.Begin();
+                    _sprite.Draw(_texture, new ColorBGRA(255, 255, 255, 255), null, new Vector3(-X, -Y, 0));
+                    _sprite.End();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Common.Render.Sprite.OnEndScene: " + e);
+                }
+            }
+
+            public override void OnPreReset()
+            {
+                _sprite.OnLostDevice();
+            }
+
+            public override void OnUnload()
+            {
+                _sprite.Dispose();
+                _texture.Dispose();
+            }
         }
 
         public class Text : RenderObject
         {
-            public int X { get; set; }
-            public int Y { get; set; }
-            public int Width { get; set; }
-            public ColorBGRA Color { get; set; }
-            public string text { get; set; }
+            private readonly Font _textFont;
             public FontDescription TextFontDescription;
-            private SharpDX.Direct3D9.Font _textFont;
 
             public Text(int x, int y, string text, int size, ColorBGRA color, string faceName = "Calibri")
             {
                 Color = color;
                 this.text = text;
 
-                _textFont = new SharpDX.Direct3D9.Font(
+                _textFont = new Font(
                     Drawing.Direct3DDevice,
                     new FontDescription
                     {
@@ -106,6 +250,12 @@ namespace LeagueSharp.Common
                         Quality = FontQuality.Default,
                     });
             }
+
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int Width { get; set; }
+            public ColorBGRA Color { get; set; }
+            public string text { get; set; }
 
             public override void OnEndScene()
             {
@@ -122,110 +272,15 @@ namespace LeagueSharp.Common
                     Console.WriteLine("Common.Render.Text.OnEndScene: " + e);
                 }
             }
-        }
 
-        public class Rectangle : RenderObject
-        {
-            public int X { get; set; }
-            public int Y { get; set; }
-            public int Width { get; set; }
-            public int Height { get; set; }
-            public ColorBGRA Color;
-
-            private readonly Line _line;
-
-            public Rectangle(int x, int y, int width, int height, ColorBGRA color)
+            public override void OnPreReset()
             {
-                X = x;
-                Y = y;
-                Width = width;
-                Height = height;
-                Color = color;
-                _line = new Line(Drawing.Direct3DDevice);
-                _line.Width = height;
+                _textFont.OnLostDevice();
             }
 
-            public override void OnEndScene()
+            public override void OnUnload()
             {
-                try
-                {
-                    if (_line.IsDisposed)
-                    {
-                        return;
-                    }
-                    _line.Begin();
-                    _line.Draw(new[] { new Vector2(X, Y + Height / 2), new Vector2(X + Width, Y + Height / 2), }, Color);
-                    _line.End();
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Common.Render.Rectangle.OnEndScene: " + e);
-                }
-            }
-        }
-
-        public class Sprite : RenderObject
-        {
-            public int X = 0;
-            public int Y = 0;
-            private int _width = 0;
-            private int _height = 0;
-
-            public int Width
-            {
-                set { _width = value; }
-                get { return _width > 0 ? _width : Bitmap.Width; }
-            }
-
-            public int Height
-            {
-                set { _height = value; }
-                get { return _height > 0 ? _height : Bitmap.Height; }
-            }
-
-            public Bitmap Bitmap { get; set; }
-
-            private Texture _texture;
-            private readonly SharpDX.Direct3D9.Sprite _sprite;
-
-            public Sprite(int x, int y, Bitmap bitmap, int width = -1, int height = -1)
-            {
-                X = x;
-                Y = y;
-                _width = width;
-                _height = height;
-                Bitmap = bitmap;
-                _sprite = new SharpDX.Direct3D9.Sprite(Drawing.Direct3DDevice);
-                _texture = Texture.FromMemory(
-                    Drawing.Direct3DDevice, (byte[]) new ImageConverter().ConvertTo(bitmap, typeof(byte[])),
-                    width != -1 ? width : bitmap.Width, height != -1 ? height : bitmap.Height, 0, Usage.None, Format.A1,
-                    Pool.Default, Filter.Default, Filter.Default, 0);
-            }
-
-            public void UpdateTextureBitmap(Bitmap newBitmap)
-            {
-                Bitmap = newBitmap;
-                _texture = Texture.FromMemory(
-                    Drawing.Direct3DDevice, (byte[]) new ImageConverter().ConvertTo(newBitmap, typeof(byte[])), Width,
-                    Height, 0, Usage.None, Format.A1, Pool.Default, Filter.Default, Filter.Default, 0);
-            }
-
-            public override void OnEndScene()
-            {
-                try
-                {
-                    if (_sprite.IsDisposed)
-                    {
-                        return;
-                    }
-                    _sprite.Begin();
-                    _sprite.Draw(_texture, new ColorBGRA(255, 255, 255, 255), null, new Vector3(-X, -Y, 0));
-                    _sprite.End();
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Common.Render.Sprite.OnEndScene: " + e);
-                }
+                _textFont.Dispose();
             }
         }
     }
