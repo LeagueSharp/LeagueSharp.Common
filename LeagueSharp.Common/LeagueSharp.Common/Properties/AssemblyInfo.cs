97b87cbf330d57f49d5b4d2875f269ced2dff4f5
Trees <treesbot@gmail.com>
2014-11-21 18:57:57 -0500

    1.0.0.7

--- LeagueSharp.Common/LeagueSharp.Common/Properties/AssemblyInfo.cs
+++ LeagueSharp.Common/LeagueSharp.Common/Properties/AssemblyInfo.cs
@@ -59,5 +59,5 @@ using System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[ assembly: AssemblyVersion("1.0.0.6") ]
-[ assembly: AssemblyFileVersion("1.0.0.6") ]
+[ assembly: AssemblyVersion("1.0.0.7") ]
+[ assembly: AssemblyFileVersion("1.0.0.7") ]
