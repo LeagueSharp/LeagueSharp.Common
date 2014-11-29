afecdf12d150666f7fb263a415d6d52329b3ecb7
Hellsing <matrix11@gmx.net>
2014-11-29 02:33:50 +0100

    Added MenuWrapper class
    MenuWrapper is an easy way to access the menu and its items.

---
+++ LeagueSharp.Common/LeagueSharp.Common/MenuWrapper.cs
@@ -0,0 +1,364 @@
+﻿#region LICENSE
+
+/*
+ Copyright 2014 - 2014 LeagueSharp
+ MenuWrapper.cs is part of LeagueSharp.Common.
+ 
+ LeagueSharp.Common is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ LeagueSharp.Common is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with LeagueSharp.Common. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+#region
+
+using System;
+using System.Text.RegularExpressions;
+
+using Color = System.Drawing.Color;
+
+#endregion
+
+namespace LeagueSharp.Common
+{
+    public class MenuWrapper
+    {
+        private SubMenu _mainMenu;
+        public SubMenu MainMenu
+        {
+            get { return _mainMenu; }
+        }
+
+        private Menu _menu;
+        private Menu _targetSelectorMenu;
+        private Menu _orbwalkerMenu;
+        private Orbwalking.Orbwalker _orbwalker;
+
+        public Menu MenuHandle
+        {
+            get { return _menu; }
+        }
+        public Menu TargetSelectorMenu
+        {
+            get { return _targetSelectorMenu; }
+        }
+        public Menu OrbwalkerMenu
+        {
+            get { return _orbwalkerMenu; }
+        }
+        public Orbwalking.Orbwalker Orbwalker
+        {
+            get { return _orbwalker; }
+        }
+
+        public MenuWrapper(string menuName, bool addTargetSelector = true, bool addOrbwalker = true)
+        {
+            // Create menu
+            _menu = new Menu(menuName, Regex.Replace(menuName.ToLower(), @"\s+", ""));
+
+            if (addTargetSelector)
+            {
+                // Target selector
+                _targetSelectorMenu = new Menu("Target Selector", "ts");
+                SimpleTs.AddToMenu(_targetSelectorMenu);
+                _menu.AddSubMenu(_targetSelectorMenu);
+            }
+
+            if (addOrbwalker)
+            {
+                // Orbwalker
+                _orbwalkerMenu = new Menu("Orbwalker", "orbwalker");
+                _orbwalker = new Orbwalking.Orbwalker(_orbwalkerMenu);
+                _menu.AddSubMenu(_orbwalkerMenu);
+            }
+
+            // Create main menu wrapper
+            _mainMenu = new SubMenu(_menu);
+
+            // Finalize menu
+            _menu.AddToMainMenu();
+        }
+
+        public class SubMenu
+        {
+            private Menu _subMenu;
+            public Menu MenuHandle
+            {
+                get { return _subMenu; }
+            }
+
+            public SubMenu(Menu current)
+            {
+                // This initializer is only for existing menus
+                _subMenu = current;
+            }
+
+            public SubMenu(Menu parent, string name)
+            {
+                // Initialize this submenu
+                _subMenu = new Menu(name, GetName(name, false));
+
+                // Add submenu to the parent menu
+                parent.AddSubMenu(_subMenu);
+            }
+
+            #region SubMenu
+
+            public SubMenu AddSubMenu(string name)
+            {
+                return new SubMenu(_subMenu, name);
+            }
+
+            public SubMenu GetSubMenu(string name)
+            {
+                return new SubMenu(_subMenu.SubMenu(GetName(name)));
+            }
+
+            #endregion
+
+            #region Boolean
+
+            public SubMenu AddBool(string name, bool defaultValue = true)
+            {
+                _subMenu.AddItem(new MenuItem(GetName(name), name).SetValue(defaultValue));
+
+                return this;
+            }
+
+            public bool GetBool(string name)
+            {
+                return _subMenu.Item(GetName(name)).GetValue<bool>();
+            }
+
+            public BoolLink CreateBoolLink(string name)
+            {
+                return new BoolLink(this, name);
+            }
+
+            #endregion
+
+            #region KeyBind
+
+            public SubMenu AddKeyBind(string name, uint key, KeyBindType type, bool defaultValue = false)
+            {
+                _subMenu.AddItem(new MenuItem(GetName(name), name).SetValue(new KeyBind(key, type, defaultValue)));
+
+                return this;
+            }
+
+            public KeyBind GetKeyBind(string name)
+            {
+                return _subMenu.Item(GetName(name)).GetValue<KeyBind>();
+            }
+
+            public KeyBindLink CreateKeyBindLink(string name)
+            {
+                return new KeyBindLink(this, name);
+            }
+
+            #endregion
+
+            #region Slider
+
+            public SubMenu AddSlider(string name, int value, int minValue, int maxValue)
+            {
+                _subMenu.AddItem(new MenuItem(GetName(name), name).SetValue(new Slider(value, minValue, maxValue)));
+
+                return this;
+            }
+
+            public Slider GetSlider(string name)
+            {
+                return _subMenu.Item(GetName(name)).GetValue<Slider>();
+            }
+
+            public SliderLink CreateSliderLink(string name)
+            {
+                return new SliderLink(this, name);
+            }
+
+            #endregion
+
+            #region StringList
+
+            public SubMenu AddStringList(string name, string[] sList, int defaultSelectedIndex = 0)
+            {
+                _subMenu.AddItem(new MenuItem(GetName(name), name).SetValue(new StringList(sList, defaultSelectedIndex)));
+
+                return this;
+            }
+
+            public StringList GetStringList(string name)
+            {
+                return _subMenu.Item(GetName(name)).GetValue<StringList>();
+            }
+
+            public StringListLink CreateStringListLink(string name)
+            {
+                return new StringListLink(this, name);
+            }
+
+            #endregion
+
+            #region Circle
+
+            public SubMenu AddCircle(string name, bool enabled, Color color, float radius = 100)
+            {
+                _subMenu.AddItem(new MenuItem(GetName(name), name).SetValue(new Circle(enabled, color, radius)));
+
+                return this;
+            }
+
+            public Circle GetCircle(string name)
+            {
+                return _subMenu.Item(GetName(name)).GetValue<Circle>();
+            }
+
+            public CircleLink CreateCircleLink(string name)
+            {
+                return new CircleLink(this, name);
+            }
+
+            #endregion
+
+            private string GetName(string name, bool fullName = true)
+            {
+                return Regex.Replace((fullName ? _subMenu.Name : "") + name.ToLower(), @"\s+", "");
+            }
+        }
+
+        #region MenuItemLinks
+
+        private class BoolLink
+        {
+            private readonly SubMenu menu;
+            private readonly string name;
+
+            public bool Value
+            {
+                get
+                {
+                    return menu.GetBool(name);
+                }
+                set
+                {
+                    menu.MenuHandle.Item(name).SetValue(value);
+                }
+            }
+
+            public BoolLink(SubMenu menu, string name)
+            {
+                this.menu = menu;
+                this.name = name;
+            }
+        }
+
+        private class KeyBindLink
+        {
+            private readonly SubMenu menu;
+            private readonly string name;
+
+            public KeyBind Value
+            {
+                get
+                {
+                    return menu.GetKeyBind(name);
+                }
+                set
+                {
+                    menu.MenuHandle.Item(name).SetValue(value);
+                }
+            }
+
+            public KeyBindLink(SubMenu menu, string name)
+            {
+                this.menu = menu;
+                this.name = name;
+            }
+        }
+
+        private class SliderLink
+        {
+            private readonly SubMenu menu;
+            private readonly string name;
+
+            public Slider Value
+            {
+                get
+                {
+                    return menu.GetSlider(name);
+                }
+                set
+                {
+                    menu.MenuHandle.Item(name).SetValue(value);
+                }
+            }
+
+            public SliderLink(SubMenu menu, string name)
+            {
+                this.menu = menu;
+                this.name = name;
+            }
+        }
+
+        private class StringListLink
+        {
+            private readonly SubMenu menu;
+            private readonly string name;
+
+            public StringList Value
+            {
+                get
+                {
+                    return menu.GetStringList(name);
+                }
+                set
+                {
+                    menu.MenuHandle.Item(name).SetValue(value);
+                }
+            }
+
+            public StringListLink(SubMenu menu, string name)
+            {
+                this.menu = menu;
+                this.name = name;
+            }
+        }
+
+        private class CircleLink
+        {
+            private readonly SubMenu menu;
+            private readonly string name;
+
+            public Circle Value
+            {
+                get
+                {
+                    return menu.GetCircle(name);
+                }
+                set
+                {
+                    menu.MenuHandle.Item(name).SetValue(value);
+                }
+            }
+
+            public CircleLink(SubMenu menu, string name)
+            {
+                this.menu = menu;
+                this.name = name;
+            }
+        }
+
+        #endregion
+    }
+}
