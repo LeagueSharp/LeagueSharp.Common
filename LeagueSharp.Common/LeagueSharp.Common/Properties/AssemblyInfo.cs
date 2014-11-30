5b9821528a9a55cd418160b5640ce58fddcbff9b
iMeh <mothejeahk@googlemail.com>
2014-11-30 18:31:30 +0100

    lol²
    lol2

--- LeagueSharp.Common/LeagueSharp.Common/Properties/AssemblyInfo.cs
+++ LeagueSharp.Common/LeagueSharp.Common/Properties/AssemblyInfo.cs
@@ -59,5 +59,5 @@ using System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[ assembly: AssemblyVersion("1.0.0.8") ]
-[ assembly: AssemblyFileVersion("1.0.0.8") ]
+[ assembly: AssemblyVersion("1.0.0.9") ]
+[ assembly: AssemblyFileVersion("1.0.0.9") ]
