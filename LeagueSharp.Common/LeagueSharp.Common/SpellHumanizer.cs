0e12e0666984bd09079be867ff8227d836c23bb7
Trees <treesbot@gmail.com>
2014-12-14 17:16:17 -0500

    Update for 4.21
    Update for 4.21

--- LeagueSharp.Common/LeagueSharp.Common/SpellHumanizer.cs
+++ LeagueSharp.Common/LeagueSharp.Common/SpellHumanizer.cs
@@ -33,19 +33,7 @@ namespace LeagueSharp.Common
 
         private static bool CanCast(GamePacket p)
         {
-            var slot = (SpellSlot) p.ReadByte(6);
-            SpellState state;
-
-            if (slot == SpellSlot.Summoner1 || slot == SpellSlot.Summoner2)
-            {
-                state = ObjectManager.Player.SummonerSpellbook.CanUseSpell(slot);
-            }
-            else
-            {
-                state = ObjectManager.Player.Spellbook.CanUseSpell(slot);
-            }
-
-            return state == SpellState.Ready;
+            return ObjectManager.Player.Spellbook.CanUseSpell((SpellSlot) p.ReadByte(6)) == SpellState.Ready;
         }
     }
 }
