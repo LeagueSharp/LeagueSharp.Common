f6bbe70c20e0019fc22b1f337256f70392cac534
Hellsing <matrix11@gmx.net>
2014-12-27 20:52:50 +0100

    Revert "Revert "Menu catch possible exceptions""
    This reverts commit 3a49ac803581da72456c52885af48c8815e39672.

--- LeagueSharp.Common/LeagueSharp.Common/Menu.cs
+++ LeagueSharp.Common/LeagueSharp.Common/Menu.cs
@@ -347,7 +347,7 @@ namespace LeagueSharp.Common
 
         private int _cachedMenuCount = -1;
         private int _cachedMenuCountT;
-        private FileStream _menuStateFileStream;
+        private FileInfo _menuStateFileInfo;
 
         private bool _visible;
 
@@ -361,13 +361,9 @@ namespace LeagueSharp.Common
             {
                 CustomEvents.Game.OnGameEnd += delegate { SaveAll(); };
                 Game.OnGameEnd += delegate { SaveAll(); };
-                AppDomain.CurrentDomain.DomainUnload += delegate
-                {
-                    SaveAll();
-                };
+                AppDomain.CurrentDomain.DomainUnload += delegate { SaveAll(); };
                 AppDomain.CurrentDomain.ProcessExit += delegate { SaveAll(); };
             }
-
         }
 
         internal int XLevel
@@ -410,14 +406,18 @@ namespace LeagueSharp.Common
 
                 int result = 0;
                 int i = 0;
-                foreach (FileInfo info in Directory.GetParent(_menuStateFileStream.Name).EnumerateFiles().OrderBy(filename => filename.Name))
+                if (_menuStateFileInfo.Directory != null)
                 {
-                    if (info.FullName == _menuStateFileStream.Name)
+                    foreach (FileInfo info in
+                        _menuStateFileInfo.Directory.EnumerateFiles().OrderBy(filename => filename.Name))
                     {
-                        result = i;
-                        break;
+                        if (info.FullName == _menuStateFileInfo.FullName)
+                        {
+                            result = i;
+                            break;
+                        }
+                        i++;
                     }
-                    i++;
                 }
 
                 _cachedMenuCount = result;
@@ -696,32 +696,42 @@ namespace LeagueSharp.Common
 
         private void InitMenuState(string assemblyName)
         {
-            string menuState = Path.Combine(
+            try
+            {
+                string menuState = Path.Combine(
                     Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "LeagueSharp", "MenuState");
-            if (!Directory.Exists(menuState))
-            {
-                Directory.CreateDirectory(menuState);
+                if (!Directory.Exists(menuState))
+                {
+                    Directory.CreateDirectory(menuState);
+                }
+                string menuStateProcess = Path.Combine(
+                    menuState, Process.GetCurrentProcess().Id.ToString(CultureInfo.InvariantCulture));
+                if (!Directory.Exists(menuStateProcess))
+                {
+                    Directory.CreateDirectory(menuStateProcess);
+                }
+                string menuStateProcessFile = Path.Combine(menuStateProcess, assemblyName);
+                _menuStateFileInfo = new FileInfo(menuStateProcessFile);
+                _menuStateFileInfo.Create();
             }
-            string menuStateProcess = Path.Combine(
-                menuState, Process.GetCurrentProcess().Id.ToString(CultureInfo.InvariantCulture));
-            if (!Directory.Exists(menuStateProcess))
+            catch (Exception e)
             {
-                Directory.CreateDirectory(menuStateProcess);
+                Console.WriteLine(e);
             }
-            string menuStateProcessFile = Path.Combine(menuStateProcess, assemblyName);
-            _menuStateFileStream = File.Open(menuStateProcessFile, FileMode.OpenOrCreate);
-            _menuStateFileStream.Close();
         }
 
         private void UnloadMenuState()
         {
-            if (_menuStateFileStream != null)
+            if (_menuStateFileInfo != null)
             {
-                if (File.Exists(_menuStateFileStream.Name))
+                try
                 {
-                    File.Delete(_menuStateFileStream.Name);
+                    _menuStateFileInfo.Delete();
                 }
-                _menuStateFileStream.Dispose();
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
             }
         }
 
@@ -743,7 +753,9 @@ namespace LeagueSharp.Common
         public MenuItem Item(string name, bool makeChampionUniq = false)
         {
             if (makeChampionUniq)
+            {
                 name = ObjectManager.Player.ChampionName + name;
+            }
 
             //Search in our own items
             foreach (var item in Items.Where(item => item.Name == name))
@@ -827,7 +839,9 @@ namespace LeagueSharp.Common
         public MenuItem(string name, string displayName, bool makeChampionUniq = false)
         {
             if (makeChampionUniq)
+            {
                 name = ObjectManager.Player.ChampionName + name;
+            }
 
             Name = name;
             DisplayName = displayName;
