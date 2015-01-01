b6854f7dc357b146997d5685ff94b1bcc46c5d38
Brian Nguyen <nemo3596@gmail.com>
2015-01-01 13:18:59 +0700

    Add Check Have Item
    Sometime i want to check i have that item or not and i dont want to know
    if it ready or not

--- LeagueSharp.Common/LeagueSharp.Common/Items.cs
+++ LeagueSharp.Common/LeagueSharp.Common/Items.cs
@@ -191,6 +191,11 @@ namespace LeagueSharp.Common
                 Range = range;
             }
 
+            public bool Have()
+            {
+                return HasItem(Id);
+            }
+
             public bool IsReady()
             {
                 return CanUseItem(Id);
