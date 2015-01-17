8a15124ed5d7f56bdadb0f86cfe23372912834e7
Hellsing <matrix11@gmx.net>
2015-01-17 17:40:22 +0100

    Added Find and FindAll extension methods for IEnumerable

--- LeagueSharp.Common/LeagueSharp.Common/Utility.cs
+++ LeagueSharp.Common/LeagueSharp.Common/Utility.cs
@@ -39,6 +39,22 @@ namespace LeagueSharp.Common
     public static class Utility
     {
         /// <summary>
+        ///     Searches for an element that matches the conditions defined by the specified predicate, and returns the first occurrence within the entire IEnumerable.
+        /// </summary>
+        public static TSource Find<TSource>(this IEnumerable<TSource> source, Predicate<TSource> match)
+        {
+            return (source is List<TSource> ? source as List<TSource> : source.ToList()).Find(match);
+        }
+
+        /// <summary>
+        ///     Retrieves all the elements that match the conditions defined by the specified predicate.
+        /// </summary>
+        public static List<TSource> FindAll<TSource>(this IEnumerable<TSource> source, Predicate<TSource> match)
+        {
+            return (source is List<TSource> ? source as List<TSource> : source.ToList()).FindAll(match);
+        }
+
+        /// <summary>
         ///     Returns if the source is facing the target.
         /// </summary>
         [Obsolete("The optional parameter lineLength will be removed, please avoid using it :-)", false)]
