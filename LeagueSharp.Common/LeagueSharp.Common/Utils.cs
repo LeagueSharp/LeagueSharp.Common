00d825b23a6bede81ee5715338272408dfd4f388
Esk0r <kortatu1512@hotmail.com>
2015-01-24 13:24:24 +0100

    Moved the linQ extensions

--- LeagueSharp.Common/LeagueSharp.Common/Utils.cs
+++ LeagueSharp.Common/LeagueSharp.Common/Utils.cs
@@ -252,6 +252,23 @@ namespace LeagueSharp.Common
 
     public static class EnumerableExtensions
     {
+        /// <summary>
+        ///     Searches for an element that matches the conditions defined by the specified predicate, and returns the first
+        ///     occurrence within the entire IEnumerable.
+        /// </summary>
+        public static TSource Find<TSource>(this IEnumerable<TSource> source, Predicate<TSource> match)
+        {
+            return (source as List<TSource> ?? source.ToList()).Find(match);
+        }
+
+        /// <summary>
+        ///     Retrieves all the elements that match the conditions defined by the specified predicate.
+        /// </summary>
+        public static List<TSource> FindAll<TSource>(this IEnumerable<TSource> source, Predicate<TSource> match)
+        {
+            return (source as List<TSource> ?? source.ToList()).FindAll(match);
+        }
+
         public static T MaxOrDefault<T, R>(this IEnumerable<T> container, Func<T, R> valuingFoo) where R : IComparable
         {
             var enumerator = container.GetEnumerator();
