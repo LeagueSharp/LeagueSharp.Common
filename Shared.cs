865806d0d0a38e57f2affbf16db4eff3f408482e
Beaving <beaving@gmail.com>
2015-02-15 01:44:32 +0100

    Added Shared.cs
    Examples on how to use it:
    
    Assembly that provides an api/interface (in this example a little baseult interface): http://privatepaste.com/5a113cf1a4
    
    Assembly that uses the interface: http://privatepaste.com/96b35dd444

---
+++ Shared.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace LeagueSharp.Common
+{
+    public static class Shared
+    {
+        #region Interface definitions
+
+
+        #endregion Interface definitions
+
+        public static InterfaceType GetInterface<InterfaceType>() where InterfaceType : class
+        {
+            try
+            {
+                return new ChannelFactory<InterfaceType>(new NetNamedPipeBinding(), new EndpointAddress("net.pipe://localhost/" + typeof(InterfaceType).Name)).CreateChannel();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Failed to connect to assembly pipe for Common.Shared communication. The targetted assembly may not be loaded yet. Desired interface: " + typeof(InterfaceType).Name, e);
+            }
+        }
+
+        public static ServiceHost ShareInterface<ImplementationType>(bool open = true)
+        {
+            ServiceHost host = new ServiceHost(typeof(ImplementationType), new Uri("net.pipe://localhost"));
+
+            host.AddServiceEndpoint(typeof(ImplementationType).GetInterfaces()[0], new NetNamedPipeBinding(), typeof(ImplementationType).GetInterfaces()[0].Name);
+
+            if (open)
+            {
+                host.Open();
+            }
+
+            return host;
+        }
+    }
+}
