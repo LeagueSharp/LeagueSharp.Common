1ea5b6bad67ded329cfee03166f8855c62a83c89
pean153 <ancinec.petr@hotmail.com>
2015-02-26 20:09:46 +0100

    Update Utils.cs

--- Utils.cs
+++ Utils.cs
@@ -29,6 +29,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
+using System.Windows.Input;
 using SharpDX;
 
 #endregion
@@ -73,13 +74,16 @@ namespace LeagueSharp.Common
         {
             return CursorPosT.GetCursorPos();
         }
-
+        public static bool IsKeyPressed(this Key key)
+        {
+            return Keyboard.IsKeyDown(key);
+        }
         public static string KeyToText(uint vKey)
         {
             /*A-Z */
             if (vKey >= 65 && vKey <= 90)
             {
-                return ((char) vKey).ToString();
+                return ((char)vKey).ToString();
             }
 
             /*F1-F12*/
@@ -144,14 +148,14 @@ namespace LeagueSharp.Common
 
         public static byte[] GetBytes(string str)
         {
-            var bytes = new byte[str.Length * sizeof (char)];
+            var bytes = new byte[str.Length * sizeof(char)];
             Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
             return bytes;
         }
 
         public static string GetString(byte[] bytes)
         {
-            var chars = new char[bytes.Length / sizeof (char)];
+            var chars = new char[bytes.Length / sizeof(char)];
             Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
             return new string(chars);
         }
@@ -187,7 +191,7 @@ namespace LeagueSharp.Common
                 var window_height = Console.WindowHeight;
                 Console.Clear();
             }
-            catch {}
+            catch { }
         }
 
         /// <summary>
@@ -243,10 +247,10 @@ namespace LeagueSharp.Common
 
             private static void Game_OnWndProc(WndEventArgs args)
             {
-                if (args.Msg == (uint) WindowsMessages.WM_MOUSEMOVE)
+                if (args.Msg == (uint)WindowsMessages.WM_MOUSEMOVE)
                 {
-                    _posX = unchecked((short) args.LParam);
-                    _posY = unchecked((short) ((long) args.LParam >> 16));
+                    _posX = unchecked((short)args.LParam);
+                    _posY = unchecked((short)((long)args.LParam >> 16));
                 }
             }
 
@@ -281,7 +285,9 @@ namespace LeagueSharp.Common
         {
             var enumerator = container.GetEnumerator();
             if (!enumerator.MoveNext())
+            {
                 return default(T);
+            }
             
             var maxElem = enumerator.Current;
             var maxVal = valuingFoo(maxElem);
