516161ec28708552e429f831dea1073dd1df7d46
soulcaliber <soulcaliber1984@gmail.com>
2015-02-26 07:02:59 -0800

    Orbwalker Stutter Fix
    fixed faulty timer that caused orbwalker to stutter

--- Utils.cs
+++ Utils.cs
@@ -61,7 +61,7 @@ namespace LeagueSharp.Common
         
         public static int TickCount
         {
-            get { return Environment.TickCount & int.MaxValue; }
+            get { return (int)(Game.ClockTime * 1000); }
         }
 
         /// <summary>
