74fcad31a9a13f7ee1e55df1ecf3ac4acf7f6cc1
h3h3 <h3h3sharp@gmail.com>
2015-04-03 00:09:14 +0200

    DynamicInitializer added

---
+++ DynamicInitializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace LeagueSharp.Common
+{
+    public class DynamicInitializer
+    {
+        public static TV NewInstance<TV>() where TV : class
+        {
+            return ObjectGenerator(typeof (TV)) as TV;
+        }
+
+        public static object NewInstance(Type type)
+        {
+            return ObjectGenerator(type);
+        }
+
+        private static object ObjectGenerator(Type type)
+        {
+            var target = type.GetConstructor(Type.EmptyTypes);
+            var dynamic = new DynamicMethod(string.Empty, type, new Type[0], target.DeclaringType);
+            var il = dynamic.GetILGenerator();
+            il.DeclareLocal(target.DeclaringType);
+            il.Emit(OpCodes.Newobj, target);
+            il.Emit(OpCodes.Stloc_0);
+            il.Emit(OpCodes.Ldloc_0);
+            il.Emit(OpCodes.Ret);
+
+            var method = (Func<object>) dynamic.CreateDelegate(typeof (Func<object>));
+            return method();
+        }
+    }
+}
