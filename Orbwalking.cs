fe6594962fab2da92ae33bf7aed5ae5698f91bde
Esk0r <kortatu1512@hotmail.com>
2015-05-15 22:36:53 +0200

    whops

--- Orbwalking.cs
+++ Orbwalking.cs
@@ -352,8 +352,11 @@ namespace LeagueSharp.Common
 
                     if (!DisableNextAttack)
                     {
-                        LastAATick = Utils.TickCount + Game.Ping + 70 - (int)(ObjectManager.Player.AttackCastDelay * 1000f);
-                        _missileLaunched = false;
+                        if (!NoCancelChamps.Contains(Player.ChampionName))
+                        {
+                            LastAATick = Utils.GameTimeTickCount + Game.Ping + 70 - (int)(ObjectManager.Player.AttackCastDelay * 1000f);
+                            _missileLaunched = false;    
+                        }
                         Player.IssueOrder(GameObjectOrder.AttackUnit, target);
                         _lastTarget = target;
                         return;
