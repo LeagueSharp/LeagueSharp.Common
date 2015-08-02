030758dfcf906427e52c4e2882e856cc267cc574
L33T <WorstPing@users.noreply.github.com>
2015-08-02 13:28:31 +0300

    Fix butcher calculations.
    Something happened.

--- Damage.cs
+++ Damage.cs
@@ -5953,7 +5953,7 @@ namespace LeagueSharp.Common
             // + Basic attacks and single target abilities do 2 bonus damage to minions and monsters. 
             if (hero != null && target is Obj_AI_Minion)
             {
-                if (hero.Masteries.Any(m => m.Page == MasteryPage.Offense && m.Id == 65 && m.Points == 1))
+                if (hero.Masteries.Any(m => m.Page == MasteryPage.Offense && m.Id == 68 && m.Points == 1))
                 {
                     value += 2d;
                 }
