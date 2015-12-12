64f2e951c821f9ace2bda15c223c5c09fabbeadc
L33T <worstping.git@gmail.com>
2015-12-12 03:39:57 +0200

    LeagueSharp.Common: Updating .gitignore and uploading missing files.

---
+++ Menu/KeyBindType.cs
@@ -0,0 +1,18 @@
+﻿namespace LeagueSharp.Common
+{
+    /// <summary>
+    ///     The KeyBind enum, representing the type of the keybind.
+    /// </summary>
+    public enum KeyBindType
+    {
+        /// <summary>
+        ///     The toggle keybind type.
+        /// </summary>
+        Toggle,
+
+        /// <summary>
+        ///     The press keybind type.
+        /// </summary>
+        Press
+    }
+}
