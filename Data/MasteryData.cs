d4c4857a40781c4b571cb0f9184d6d930f91da02
h3h3 <h3h3.staff@joduska.me>
2015-12-19 15:24:41 +0100

    changed MasteryData protection level to public

--- Data/MasteryData.cs
+++ Data/MasteryData.cs
@@ -30,39 +30,33 @@ namespace LeagueSharp.Common.Data
 {
     public static class MasteryData
     {
-        internal static Mastery FindMastery(this Obj_AI_Hero @hero, MasteryPage page, int id)
+        public static Mastery FindMastery(this Obj_AI_Hero @hero, MasteryPage page, int id)
         {
             var mastery = @hero.Masteries.FirstOrDefault(m => m.Page == page && m.Id == id);
             return mastery;
         }
 
-        internal static Mastery GetMastery(this Obj_AI_Hero hero, Ferocity ferocity)
+        public static Mastery GetMastery(this Obj_AI_Hero hero, Ferocity ferocity)
         {
             return FindMastery(hero, MasteryPage.Ferocity, (int)ferocity);
         }
 
-        internal static Mastery GetMastery(this Obj_AI_Hero hero, Cunning cunning)
+        public static Mastery GetMastery(this Obj_AI_Hero hero, Cunning cunning)
         {
             return FindMastery(hero, MasteryPage.Cunning, (int)cunning);
         }
 
-        internal static Mastery GetMastery(this Obj_AI_Hero hero, Resolve resolve)
+        public static Mastery GetMastery(this Obj_AI_Hero hero, Resolve resolve)
         {
             return FindMastery(hero, MasteryPage.Resolve, (int)resolve);
         }
 
-        internal static bool IsActive(this Mastery mastery)
+        public static bool IsActive(this Mastery mastery)
         {
             return mastery.Points >= 1;
         }
 
-        internal static bool IsMoveImpaired(this Obj_AI_Hero hero)
-        {
-            return hero.HasBuffOfType(BuffType.Fear) || hero.HasBuffOfType(BuffType.Slow) || hero.HasBuffOfType(BuffType.Snare) || hero.HasBuffOfType(BuffType.Stun) ||
-                   hero.HasBuffOfType(BuffType.Taunt);
-        }
-
-        internal enum Ferocity
+        public enum Ferocity
         {
             Fury = 65,
             Sorcery = 68,
@@ -79,7 +73,7 @@ namespace LeagueSharp.Common.Data
             DeathFireTouch = 137
         }
 
-        internal enum Cunning
+        public enum Cunning
         {
             Wanderer = 65,
             Savagery = 66,
@@ -96,7 +90,7 @@ namespace LeagueSharp.Common.Data
             WindspeakerBlessing = 147
         }
 
-        internal enum Resolve
+        public enum Resolve
         {
             Recovery = 65,
             Unyielding = 66,
