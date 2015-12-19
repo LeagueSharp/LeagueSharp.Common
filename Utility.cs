a0bd704c58bfa473d00fb27f1a063f520528882f
jQuery <nathan@zavox.nl>
2015-12-19 22:19:56 +0100

    Update Utility.cs

--- Utility.cs
+++ Utility.cs
@@ -105,7 +105,7 @@ namespace LeagueSharp.Common
         {
             return hero.HasBuffOfType(BuffType.Flee) || hero.HasBuffOfType(BuffType.Slow)
                    || hero.HasBuffOfType(BuffType.Snare) || hero.HasBuffOfType(BuffType.Stun)
-                   || hero.HasBuffOfType(BuffType.Taunt);
+                   || hero.HasBuffOfType(BuffType.Taunt) || hero.HasBuffOfType(BuffType.Charm);
         }
 
         /// <summary>
