bdc77a59666fbb62d80e9c84f33157142547de39
Exory <drago.enrico@hotmail.it>
2016-02-11 15:24:47 +0100

    Update Orbwalking.cs
    Added Jhin's Q to AutoAttack-Resets.

--- Orbwalking.cs
+++ Orbwalking.cs
@@ -119,7 +119,7 @@ namespace LeagueSharp.Common
             "gangplankqwrapper", "poppypassiveattack", "powerfist", "renektonpreexecute", "rengarq",
             "shyvanadoubleattack", "sivirw", "takedown", "talonnoxiandiplomacy", "trundletrollsmash", "vaynetumble",
             "vie", "volibearq", "xenzhaocombotarget", "yorickspectral", "reksaiq", "itemtitanichydracleave", "masochism",
-            "illaoiw"
+            "illaoiw", "jhinq"
         };
 
 
@@ -447,7 +447,7 @@ namespace LeagueSharp.Common
 
             if (Player.ChampionName == "Jhin")
             {
-                if(Player.HasBuff("JhinPassiveReload"))
+                if (Player.HasBuff("JhinPassiveReload"))
                 {
                     return false;
                 }
