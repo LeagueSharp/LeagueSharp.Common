77ffbfae712bf282d69d2fd2ed890a42d1425d2e
h3h3 <h3h3.staff@joduska.me>
2016-07-31 23:46:07 +0200

    updated hacks

--- Hacks.cs
+++ Hacks.cs
@@ -9,6 +9,18 @@ namespace LeagueSharp.Common
     {
         private static Menu menu;
 
+        private static MenuItem MenuAntiAfk;
+
+        private static MenuItem MenuDisableDrawings;
+
+        private static MenuItem MenuDisableSay;
+
+        private static MenuItem MenuTowerRange;
+
+        private const int WM_KEYDOWN = 0x100;
+
+        private const int WM_KEYUP = 0x101;
+
         /// <summary>
         /// Initializes this instance.
         /// </summary>
@@ -18,44 +30,46 @@ namespace LeagueSharp.Common
             {
                 menu = new Menu("Hacks", "Hacks");
 
-                var draw = menu.AddItem(new MenuItem("DrawingHack", "Disable Drawing").SetValue(false));
-                draw.SetValue(LeagueSharp.Hacks.DisableDrawings);
-                draw.ValueChanged +=
-                    delegate(object sender, OnValueChangeEventArgs args)
-                    {
-                        LeagueSharp.Hacks.DisableDrawings = args.GetNewValue<bool>();
-                    };
+                MenuAntiAfk = menu.AddItem(new MenuItem("AfkHack", "Anti-AFK").SetValue(false));
+                MenuAntiAfk.ValueChanged += (sender, args) => LeagueSharp.Hacks.AntiAFK = args.GetNewValue<bool>();
 
-                var say = menu.AddItem(new MenuItem("SayHack", "Disable L# Send Chat").SetValue(false)
-                    .SetTooltip("Block Game.Say from Assemblies"));
-                say.SetValue(LeagueSharp.Hacks.DisableSay);
-                say.ValueChanged +=
-                    delegate(object sender, OnValueChangeEventArgs args)
-                    {
-                        LeagueSharp.Hacks.DisableSay = args.GetNewValue<bool>();
-                    };
+                MenuDisableDrawings = menu.AddItem(new MenuItem("DrawingHack", "Disable Drawing").SetValue(false));
+                MenuDisableDrawings.ValueChanged += (sender, args) => LeagueSharp.Hacks.DisableDrawings = args.GetNewValue<bool>();
 
-                /*  var zoom = menu.AddItem(new MenuItem("ZoomHack", "Extended Zoom").SetValue(false));
-                zoom.SetValue(LeagueSharp.Hacks.ZoomHack);
-                zoom.ValueChanged +=
-                    delegate (object sender, OnValueChangeEventArgs args)
-                    {
-                        LeagueSharp.Hacks.ZoomHack = args.GetNewValue<bool>();
-                    };
+                MenuDisableSay = menu.AddItem(new MenuItem("SayHack", "Disable L# Send Chat").SetValue(false).SetTooltip("Block Game.Say from Assemblies"));
+                MenuDisableSay.ValueChanged += (sender, args) => LeagueSharp.Hacks.DisableSay = args.GetNewValue<bool>();
 
-                menu.AddItem(
-                    new MenuItem("ZoomHackInfo", "Note: ZoomHack may be unsafe!", false, FontStyle.Regular, Color.Red));
-                */
+                MenuTowerRange = menu.AddItem(new MenuItem("TowerHack", "Show Tower Ranges").SetValue(false));
+                MenuTowerRange.ValueChanged += (sender, args) => LeagueSharp.Hacks.TowerRanges = args.GetNewValue<bool>();
 
-                var tower = menu.AddItem(new MenuItem("TowerHack", "Show Tower Ranges").SetValue(false));
-                tower.SetValue(LeagueSharp.Hacks.TowerRanges);
-                tower.ValueChanged +=
-                    delegate(object sender, OnValueChangeEventArgs args)
-                    {
-                        LeagueSharp.Hacks.TowerRanges = args.GetNewValue<bool>();
-                    };
+                LeagueSharp.Hacks.AntiAFK = MenuAntiAfk.GetValue<bool>();
+                LeagueSharp.Hacks.DisableSay = MenuDisableSay.GetValue<bool>();
+                LeagueSharp.Hacks.TowerRanges = MenuTowerRange.GetValue<bool>();
 
                 CommonMenu.Instance.AddSubMenu(menu);
+
+                Game.OnWndProc += args =>
+                {
+                    if (!MenuDisableDrawings.GetValue<bool>())
+                    {
+                        return;
+                    }
+
+                    if ((int)args.WParam != Config.ShowMenuPressKey)
+                    {
+                        return;
+                    }
+
+                    if (args.Msg == WM_KEYDOWN)
+                    {
+                        LeagueSharp.Hacks.DisableDrawings = false;
+                    }
+
+                    if (args.Msg == WM_KEYUP)
+                    {
+                        LeagueSharp.Hacks.DisableDrawings = true;
+                    }
+                };
             };
         }
 
